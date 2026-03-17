9b605abf8dcf0b1a4cf5aa8f4b22676a66f32ffd
Visionnn !!! <visionnn097@gmail.com>
2026-03-18 01:12:26 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Services from './components/Services'
+import Skills from './components/Skills'
+import Footer from './components/Footer'
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      offset: 100,
+      once: true
+    })
+  }, [])
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Services />
+      <Skills />
+      <Footer />
+    </>
+  )
+}
+
+export default App
 
