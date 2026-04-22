4b681f28b4e963ec67bd5e053d04a2e6ba50ee19
Visionnn !!! <visionnn097@gmail.com>
2026-04-23 00:28:11 +0545

    feat: compose portfolio layout with ThemeProvider and section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Services from './components/Services'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Services />
+      <Skills />
+      <Contact />
+      <Footer />
+    </ThemeProvider>
+  )
+}
+
+export default App
 
